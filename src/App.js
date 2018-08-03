07f1dd29e3f332b7c874fa4f763995404676ed85
hackjutsu <cosmo.lepton@gmail.com>
2018-08-02 20:54:33 -0700

    user enter the lottery

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
   state = {
     manager: '',
     players: [],
-    balance: ''
+    balance: '',
+    value: '',
+    message:''
   };
 
   async componentDidMount() {
@@ -18,6 +20,20 @@ class App extends Component {
     this.setState({ manager, players, balance });
   }
 
+  onSubmit = async (event) => {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({message: 'Waiting on transaction success...'});
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    });
+
+    this.setState({message: 'You have been entered!'});
+  };
+
   render() {
     return (
       <div>
@@ -27,6 +43,20 @@ class App extends Component {
           There are currently {this.state.players.length} people entered,
           competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
         </p>
+        <hr/>
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
+        <hr/>
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
